b45e65019a6c3789f49b47dd3cd9b8b6a0522c54
Gaeun-Kimm <ge_kim@seoultech.ac.kr>
2025-08-20 10:18:43 +0900

    Add project code and assets

--- scripts/teacher_to_bevdepth4d/mvp2bevdepth.sh
+++ scripts/teacher_to_bevdepth4d/mvp2bevdepth.sh
@@ -5,8 +5,8 @@ export LIBRARY_PATH=$CUDA_HOME/lib64:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 export CFLAGS="-I$CUDA_HOME/include $CFLAGS"
 
-filename=centerpoint2bevdepth.sh
-logdir=outputs/centerpoint_to_bevdepth4d_r50/entropy_minimization+as_b32/
+filename=mvp2bevdepth.sh
+logdir=outputs/
 
 
 # cd /mnt/vepfs/ML/ml-users/dingwen/code/DistillBEV
@@ -16,16 +16,16 @@ mkdir -p ${logdir}
 cat ./scripts/teacher_to_bevdepth4d/$filename > ${logdir}run.sh
 
 ############
-cp ./tools/epoch_based_runner_modified.py /root/miniconda3/envs/distillbev/lib/python3.8/site-packages/mmcv/runner/epoch_based_runner.py
-cp ./tools/tensorboard_modified.py /root/miniconda3/envs/distillbev/lib/python3.8/site-packages/mmcv/runner/hooks/logger/tensorboard.py
+cp ./tools/epoch_based_runner_modified.py /opt/conda/lib/python3.8/site-packages/mmcv/runner/epoch_based_runner.py
+cp ./tools/tensorboard_modified.py /opt/conda/lib/python3.8/site-packages/mmcv/runner/hooks/logger/tensorboard.py
 ############
 
-./tools/multi_node_dist_train.sh configs/lidar2camera_bev_distillation/centerpoint_pillar_to_bevdepth4d_r50/centerpoint_02pillar_second_secfpn_circlenms_8x4_cyclic_20e_nus_to_bevdepth4d_r50.py \
+./tools/multi_node_dist_train.sh configs/lidar2camera_bev_distillation/centerpoint_pillar_to_bevdepth4d_r50/centerpoint_02pillar_second_secfpn_circlenms_8x4_cyclic_20e_nus_to_bevdepth4d_r50_virtual.py \
 --cfg-options model.inherit_head=True \
 model.img_bev_encoder_neck.extra_norm_act=True \
 data.val.prev_only=True data.test.prev_only=True \
-model.teacher_config='configs/dynamic_centerpoint/dynamic_centerpoint_02pillar_second_secfpn_4x8_cyclic_20e_nus.py' \
-model.teacher_ckpt='/root/gekim/distill-bev/ckpt/centerpoint_epoch_20.pth' \
+model.teacher_config='configs/mvp/mvp_dynamic_centerpoint_02pillar_second_secfpn_4x8_cyclic_20e_nus.py' \
+model.teacher_ckpt='ckpt/mvp_epoch_20.pth' \
 model.distill_params.spatial_attentions=['teacher_student',] \
 model.distill_params.foreground_mask='gt' model.distill_params.background_mask='logical_not' \
 model.distill_params.scale_mask='combine_gt' \
@@ -40,10 +40,10 @@ model.distill_params.fp_weight=6e-2 model.distill_params.fp_scale_mode='average'
 model.distill_params.fg_feat_loss_weights=[6e-3,] \
 model.distill_params.bg_feat_loss_weights=[4e-2,] \
 model.distill_params.channel_mask=False \
-model.img_backbone.pretrained='/root/gekim/distill-bev/ckpt/resnet50-19c8e357.pth' \
+model.img_backbone.pretrained='ckpt/resnet50-19c8e357.pth' \
 optimizer_config._delete_=True optimizer_config.grad_clip.max_norm=5 optimizer_config.grad_clip.norm_type=2 \
 optimizer.lr=2e-4 \
-checkpoint_config.interval=2 \
+checkpoint_config.interval=4 \
 --work-dir ${logdir}
 
 cat ./scripts/teacher_to_bevdepth4d/$filename
